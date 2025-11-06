a3536d6dff10b8bd81d4d52ad1fde18b47367699
ElmirKuba <elmir_kuba@vk.com>
2025-11-06 23:50:00 +0500

    pwa: тестирование айфон

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import './App.css';
 
 /** Основной компонент приложения */
 function App() {
-  return <div className="App">Это самое простое PWA приложение</div>;
+  return (
+    <div className="App">
+      Это самое простое PWA приложение | Проверка обновления на айфоне
+    </div>
+  );
 }
 
 export default App;
