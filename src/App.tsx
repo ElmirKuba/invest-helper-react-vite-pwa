c599140eb99d973632aec7cb3dcb18b987f27655
ElmirKuba <elmir_kuba@vk.com>
2025-11-06 23:44:42 +0500

    pwa: тестирование

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@ import './App.css';
 
 /** Основной компонент приложения */
 function App() {
-  return (
-    <div className="App">Привет, я должен работать как PWA. Наверное...</div>
-  );
+  return <div className="App">Это самое простое PWA приложение</div>;
 }
 
 export default App;
