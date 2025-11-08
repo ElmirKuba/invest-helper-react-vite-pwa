fab976f5c39a0af8fe48036f4ae11896fd7045d9
ElmirKuba <elmir_kuba@vk.com>
2025-11-08 12:25:47 +0500

    Проверяем обновление прилы 4

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <div>Текст: {count}</div>
+      <div> новый текст </div>
       <button
         onClick={() => {
           setCount(count + 1);
