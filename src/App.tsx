82831c4a82afa6b7a2214d1090e20e43ae832970
ElmirKuba <elmir_kuba@vk.com>
2025-11-17 20:53:24 +0500

    pwa: тестирование 23

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <StrictMode>
       <div className="App">
-        <div>Счетчик: {count}</div>
+        <div>Счетчик: {count} (текущее кол-во)</div>
         <br />
         <br />
         <div>Произошло обновление функционала: 1</div>
