dd974711e3cf01d74c8b2ade4604a9035841b253
ElmirKuba <elmir_kuba@vk.com>
2025-11-18 00:15:14 +0500

    pwa: тестирование 40

--- src/app/components/app.component.tsx
+++ src/app/components/app.component.tsx
@@ -70,10 +70,6 @@ export const AppComponent = () => {
         <div>unixtime последнего использования: {lastUsedUnixTime}</div>
         <br />
         <br />
-        <div>Мы победили это долбанное PWA:D!!!</div>
-        <div>А вот такое кто-то ожидал, а?</div>
-        <br />
-        <br />
         <button
           onClick={() => {
             setCount(count + 1);
